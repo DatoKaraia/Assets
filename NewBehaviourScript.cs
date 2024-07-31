c6e3564e6411cac8d3ce98da6e6288bb4d95c839
Dato Karaya <datokaraya@gmail.com>
2024-07-31 15:15:29 +0100

    Update NewBehaviourScript.cs

--- NewBehaviourScript.cs
+++ NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //wowo
+        //wowo Dato
     }
 
     // Update is called once per frame
