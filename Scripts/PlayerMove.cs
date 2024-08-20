7ca16e544c748437b33e751eb3f21178b31b8879
Dato Karaya <datokaraya@gmail.com>
2024-08-20 13:47:34 +0100

    Player Movement

--- Scripts/PlayerMove.cs
+++ Scripts/PlayerMove.cs
@@ -2,32 +2,114 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
 public class PlayerMove : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float walkSpeed = 3.0f;
+    public float runSpeed = 6.0f;
     public float jumpForce = 5.0f;
-    public float gravity = 9.8f;
-    public CharacterController controller;
+    public float lookSpeed = 10.0f;
+    public Camera playerCamera;
 
-    private Vector3 moveDirection = Vector3.zero;
+    private Rigidbody rb;
+    private Animator animator;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // We handle rotation manually
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
-        if (controller.isGrounded)
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
+        Move();
+        LookAtMouse();
+        Jump();
 
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpForce;
-            }
-        }
+        // Update animator parameters
+        float speed = rb.velocity.magnitude;
+        float horizontalInput = Input.GetAxis("Horizontal");
 
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+        animator.SetFloat("Speed", speed);
+        animator.SetBool("IsGrounded", isGrounded);
+        animator.SetFloat("Turn", horizontalInput);
+
+        HandleTurning(horizontalInput);
     }
 
+    void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
 
+        // Check if the player is running
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        // Set the speed based on whether the player is running or walking
+        float moveSpeed = isRunning ? runSpeed : walkSpeed;
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        move *= moveSpeed;
+
+        Vector3 velocity = rb.velocity;
+        velocity.x = move.x;
+        velocity.z = move.z;
+        rb.velocity = velocity;
+
+        // Update the animator with the running state
+        animator.SetBool("IsRunning", isRunning);
+    }
+
+    void LookAtMouse()
+    {
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(ray, out rayLength))
+        {
+            Vector3 pointToLook = ray.GetPoint(rayLength);
+            Vector3 lookDirection = pointToLook - transform.position;
+            lookDirection.y = 0; // Keep the player level with the ground
+
+            Quaternion lookRotation = Quaternion.LookRotation(lookDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lookSpeed);
+        }
+    }
+
+    void Jump()
+    {
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void HandleTurning(float horizontalInput)
+    {
+        // Example of handling turn animations
+        if (horizontalInput < -0.1f)
+        {
+            // Trigger the LeftTurn animation
+            animator.SetTrigger("LeftTurn");
+        }
+        else if (horizontalInput > 0.1f)
+        {
+            // Trigger the RightTurn animation
+            animator.SetTrigger("RightTurn");
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
 }
